2c19545762d64e1d8e74b4edea9bae2ea249ceb2
jwvg0425 <jwvg0425@naver.com>
2017-12-09 17:50:53 +0900

    update dinic

--- Dinic.cpp
+++ Dinic.cpp
@@ -1,14 +1,50 @@
+struct Edge
+{
+	int end, capa, flow;
+	Edge* dual;
+
+	Edge() : Edge(-1, 0) {}
+	Edge(int e, int c) : end(e), capa(c), flow(0), dual(nullptr) {}
+
+	int remain()
+	{
+		return capa - flow;
+	}
+
+	void addFlow(int f)
+	{
+		flow += f;
+		dual->flow -= f;
+	}
+};
+
 template<int Size>
 class Dinic
 {
 public:
-	//a,b간에 capacity c인 엣지 추가
-	void addEdge(int a, int b, int c1, int c2 = 0)
+	~Dinic()
 	{
-		capa[a][b] = c1;
-		capa[b][a] = c2;
-		adj[a].push_back(b);
-		adj[b].push_back(a);
+		for (auto& a : adj)
+		{
+			for (auto& e : a)
+			{
+				delete e;
+			}
+		}
+	}
+
+	//a,b간에 capacity c인 엣지 추가
+	Edge* addEdge(int a, int b, int c)
+	{
+		auto e1 = new Edge(b, c);
+		auto e2 = new Edge(a, 0);
+		e1->dual = e2;
+		e2->dual = e1;
+
+		adj[a].push_back(e1);
+		adj[b].push_back(e2);
+
+		return e1;
 	}
 
 	int maxFlow(int source, int sink)
@@ -33,10 +69,19 @@ public:
 		return totalFlow;
 	}
 
+	int edgeCount(int s)
+	{
+		return adj[s].size();
+	}
+
+	Edge* getEdge(int s, int i)
+	{
+		return adj[s][i];
+	}
+
 private:
-	std::vector<int> adj[Size];
-	int flow[Size][Size];
-	int capa[Size][Size];
+
+	vector<Edge*> adj[Size];
 	int level[Size];
 	int work[Size];
 
@@ -56,11 +101,11 @@ private:
 
 			for (auto& e : adj[now])
 			{
-				if (level[e] != -1 || capa[now][e] - flow[now][e] <= 0)
+				if (level[e->end] != -1 || e->remain() <= 0)
 					continue;
 
-				level[e] = level[now] + 1;
-				q.push(e);
+				level[e->end] = level[now] + 1;
+				q.push(e->end);
 			}
 		}
 
@@ -74,17 +119,16 @@ private:
 
 		for (int& i = work[s]; i < adj[s].size(); i++)
 		{
-			int next = adj[s][i];
+			auto& next = adj[s][i];
 
-			if (level[next] != level[s] + 1 || capa[s][next] - flow[s][next] <= 0)
+			if (level[next->end] != level[s] + 1 || next->remain() <= 0)
 				continue;
 
-			int df = dfs(next, e, std::min(f, capa[s][next] - flow[s][next]));
+			int df = dfs(next->end, e, std::min(f, next->remain()));
 
 			if (df != 0)
 			{
-				flow[s][next] += df;
-				flow[next][s] -= df;
+				next->addFlow(df);
 				return df;
 			}
 		}
