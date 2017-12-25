95c0b31a67aa8a991a84c0b8251464807dc07995
jwvg0425 <jwvg0425@naver.com>
2017-12-25 22:35:35 +0900

    add Default.cpp

---
+++ Default.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <bitset>
+#include <map>
+#include <set>
+#include <tuple>
+#include <string.h>
+#include <math.h>
+#include <random>
+#include <functional>
+
+using namespace std;
+
+using i64 = long long int;
+using ii = pair<int, int>;
+using ii64 = pair<i64, i64>;
