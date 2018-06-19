ac9f9f40fdc9fbf0be3b0eeb145f24f9c0c2c0d0
jwvg0425 <jwvg0425@naver.com>
2018-06-19 20:20:20 +0900

    fenwick search(log^2n)

--- cpp/Fenwick.cpp
+++ cpp/Fenwick.cpp
@@ -1,33 +1,58 @@
 class FenwickTree
 {
 public:
-	FenwickTree(int k)
-	{
-		data.resize(k);
-	}
+    FenwickTree(int k)
+    {
+        data.resize(k);
+    }
 
-	i64 sum(int n)
-	{
-		i64 ans = 0;
+    i64 sum(int n)
+    {
+        i64 ans = 0;
 
-		while (n > 0)
-		{
-			ans += data[n];
-			n -= (n & -n);
-		}
+        while (n > 0)
+        {
+            ans += data[n];
+            n -= (n & -n);
+        }
 
-		return ans;
-	}
+        return ans;
+    }
 
-	void add(int n, i64 num)
-	{
-		while (n < data.size())
-		{
-			data[n] += num;
-			n += (n & -n);
-		}
-	}
+    void add(int n, i64 num)
+    {
+        while (n < data.size())
+        {
+            data[n] += num;
+            n += (n & -n);
+        }
+    }
+
+    int search(i64 k)
+    {
+        int lo = 0, hi = data.size() - 1;
+        int ans = 0;
+
+        while (lo <= hi)
+        {
+            int mid = (lo + hi) / 2;
+
+            i64 q = sum(mid);
+
+            if (q >= k)
+            {
+                ans = mid;
+                hi = mid - 1;
+            }
+            else
+            {
+                lo = mid + 1;
+            }
+        }
+
+        return ans;
+    }
 
 private:
-	vector<i64> data;
+    vector<i64> data;
 };
