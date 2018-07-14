ffffe34065613d8c5cff29a0b98852c9b4264f67
jwvg0425 <jwvg0425@naver.com>
2018-07-15 00:16:41 +0900

    change segment tree implementation

--- cpp/SegmentTree.cpp
+++ cpp/SegmentTree.cpp
@@ -1,72 +1,60 @@
-template<typename T>
+constexpr int clog2(int n) { return ((n < 2) ? 1 : 1 + clog2(n / 2)); }
+
+template<typename T, typename Merge>
 class SegmentTree
 {
 public:
-    void init(vector<T>& raw)
-    {
-        assert(!is_init);
-        is_init = true;
-        n = raw.size();
-        int h = (int)ceil(log2(n));
-        int tree_size = (1 << (h + 1));
-        data.resize(tree_size);
+    SegmentTree(Merge m)
+        :merge(m) {}
 
-        init_internal(raw, 1, 0, n - 1);
+    void init(vector<T>& raw_)
+    {
+        raw = raw_;
+        n = (int)raw.size();
+        int sz = (1 << (clog2(n) + 1));
+        data.resize(sz);
+
+        _init(raw, 1, 0, n - 1);
     }
 
-    T update(int idx, const T& newVal)
-    {
-        assert(is_init);
-        return update_internal(1, 0, n - 1, idx, newVal);
-    }
-
-    T query(int left, int right)
-    {
-        assert(is_init);
-        return query_internal(1, 0, n - 1, left, right);
-    }
-
-    virtual T merge(const T& left, const T& right) = 0;
-    virtual T merge_with_idx(const T& left, const T& right, int left_idx, int right_idx)
-    {
-        return merge(left, right);
-    }
+    T add(int idx, const T& newVal) { raw[idx] += newVal; return update(idx, raw[idx]); }
+    T update(int idx, const T& newVal) { raw[idx] = newVal; return _update(1, 0, n - 1, idx, newVal); }
+    T query(int left, int right) { return _query(1, 0, n - 1, left, right); }
 
 private:
+    vector<T> raw;
     vector<T> data;
     int n;
-    bool is_init = false;
+    Merge merge;
 
-    T init_internal(vector<T>& raw, int node, int start, int end)
+    T _init(vector<T>& raw, int node, int start, int end)
     {
         int mid = (start + end) / 2;
         if (start == end)
             return data[node] = raw[start];
         else
-            return data[node] = merge_with_idx(init_internal(raw, node * 2, start, mid),
-                init_internal(raw, node * 2 + 1, mid + 1, end), node * 2, node * 2 + 1);
+            return data[node] = merge(_init(raw, node * 2, start, mid),
+                _init(raw, node * 2 + 1, mid + 1, end));
     }
 
-    T update_internal(int node, int start, int end, int index, const T& newVal)
+    T _update(int node, int start, int end, int index, const T& newVal)
     {
         if (index < start || index > end)
             return data[node];
 
         if (start == end)
-        {
             data[node] = newVal;
-        }
         else
         {
             int mid = (start + end) / 2;
-            data[node] = merge_with_idx(update_internal(node * 2, start, mid, index, newVal),
-                update_internal(node * 2 + 1, mid + 1, end, index, newVal), node * 2, node * 2 + 1);
+            data[node] = merge(_update(node * 2, start, mid, index, newVal),
+                _update(node * 2 + 1, mid + 1, end, index, newVal));
         }
 
         return data[node];
     }
 
-    T query_internal(int node, int start, int end, int left, int right)
+    T _query(int node, int start, int end, int left, int right)
     {
         if (left <= start && end <= right)
             return data[node];
@@ -74,12 +62,18 @@ private:
         int mid = (start + end) / 2;
 
         if (mid < left)
-            return query_internal(node * 2 + 1, mid + 1, end, left, right);
+            return _query(node * 2 + 1, mid + 1, end, left, right);
 
         if (mid + 1 > right)
-            return query_internal(node * 2, start, mid, left, right);
+            return _query(node * 2, start, mid, left, right);
 
-        return merge_with_idx(query_internal(node * 2, start, mid, left, right),
-            query_internal(node * 2 + 1, mid + 1, end, left, right), node * 2, node * 2 + 1);
+        return merge(_query(node * 2, start, mid, left, right),
+            _query(node * 2 + 1, mid + 1, end, left, right));
     }
 };
+
+template<typename T, typename Merge>
+SegmentTree<T, Merge> segTree(Merge merge)
+{
+    return SegmentTree<T, Merge>(merge);
+}
