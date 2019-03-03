db64473e84f737def291639446ed2b3fa0464ea9
namhyunuk <jwvg0425@naver.com>
2019-03-04 01:05:09 +0900

    read_list, read_string

--- rust/default.rs
+++ rust/default.rs
@@ -1,10 +1,15 @@
 use std::io;
 
-fn read_line<T>() -> Vec<T>
+fn read_list<T>() -> Vec<T>
     where T: std::str::FromStr,
-          <T as std::str::FromStr>::Err: std::fmt::Debug
-{
+          <T as std::str::FromStr>::Err: std::fmt::Debug {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
     input.as_mut_str().split_whitespace().map(|word| word.parse().unwrap()).collect()
+}
+
+fn read_string() -> Vec<char> {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    input.chars().collect()
 }
