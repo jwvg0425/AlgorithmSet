e0f97685677585aaf707d18c6df7574716c309b8
namhyunuk <jwvg0425@naver.com>
2019-11-18 23:19:01 +0900

    add pq

--- cpp/Default.cpp
+++ cpp/Default.cpp
@@ -20,6 +20,8 @@
 
 using namespace std;
 
+template<typename T, typename Pr = less<T>>
+using pq = priority_queue<T, vector<T>, Pr>;
 using i64 = long long int;
 using ii = pair<int, int>;
 using ii64 = pair<i64, i64>;
