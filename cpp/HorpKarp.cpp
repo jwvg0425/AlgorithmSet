c13ec5e5a20346e915b218b2ba69a7f863f10a6c
namhyunuk <jwvg0425@naver.com>
2020-08-22 13:52:21 +0900

    add horpcroft karp

---
+++ cpp/HorpKarp.cpp
@@ -0,0 +1,99 @@
+
+template<int Size>
+class HorpKarp
+{
+public:
+    HorpKarp() : n(0)
+    {
+    }
+
+    void set_size(int n_)
+    {
+        n = n_;
+    }
+
+    void add_edge(int s, int e)
+    {
+        adj[s].push_back(e);
+    }
+
+    int matching()
+    {
+        int res = 0;
+        memset(a, -1, sizeof(a));
+        memset(b, -1, sizeof(b));
+
+        while (true)
+        {
+            bfs();
+
+            int flow = 0;
+            for (int i = 0; i <= n; i++)
+                if (!used[i] && dfs(i))
+                    flow++;
+
+            if (flow == 0)
+                break;
+
+            res += flow;
+        }
+
+        return res;
+    }
+
+    int a[Size], b[Size];
+
+private:
+    const int INF = 1 << 30;
+    int n;
+    int dist[Size];
+    bool used[Size];
+    vector<int> adj[Size];
+
+    void bfs()
+    {
+        queue<int> q;
+        
+        for (int i = 0; i <= n; i++)
+        {
+            if (!used[i])
+            {
+                dist[i] = 0;
+                q.push(i);
+                continue;
+            }
+            
+            dist[i] = INF;
+        }
+
+        while (!q.empty())
+        {
+            int s= q.front();
+            q.pop();
+            for (int e : adj[s])
+            {
+                if (b[e] != -1 && dist[b[e]] == INF)
+                {
+                    dist[b[e]] = dist[s] + 1;
+                    q.push(b[e]);
+                }
+            }
+        }
+    }
+
+    bool dfs(int s)
+    {
+        for (int e : adj[s])
+        {
+            if (b[e] == -1 || dist[b[e]] == dist[s] + 1 && dfs(b[e]))
+            {
+                used[s] = true;
+                a[s] = e;
+                b[e] = s;
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
