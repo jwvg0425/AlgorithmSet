6db29d05f4f88a74a7aedbf63c845767bab754e3
namhyunuk <jwvg0425@naver.com>
2020-09-02 20:33:17 +0900

    add ccw

--- cpp/Geometry.cpp
+++ cpp/Geometry.cpp
@@ -20,3 +20,17 @@ struct point
 using ipoint = point<int>;
 using i64point = point<i64>;
 using dpoint = point<double>;
+
+i64 ccw(ii64 p1, ii64 p2, ii64 p3)
+{
+    i64 tmp = p1.xx * p2.yy + p2.xx * p3.yy + p3.xx * p1.yy;
+    tmp -= p1.yy * p2.xx + p2.yy * p3.xx + p3.yy * p1.xx;
+
+    if (tmp > 0)
+        return 1;
+
+    if (tmp < 0)
+        return -1;
+
+    return 0;
+}
