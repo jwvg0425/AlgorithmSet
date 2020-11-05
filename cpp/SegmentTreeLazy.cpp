d3ed34aaa5f503de80767e130f52ac2a77540cd1
namhyunuk <jwvg0425@naver.com>
2020-11-05 13:25:01 +0900

    Update SegmentTreeLazy.cpp

--- cpp/SegmentTreeLazy.cpp
+++ cpp/SegmentTreeLazy.cpp
@@ -1,93 +1,98 @@
+
 constexpr int clog2(int n) { return ((n < 2) ? 1 : 1 + clog2(n / 2)); }
 
+template<typename T, typename L>
 class SegTreeLazy
 {
 public:
-	void init(vector<i64> arr)
-	{
-		n = (int)arr.size();
-		int sz = (1 << (clog2(n) + 1));
-		tree.resize(sz);
-		lazy.resize(sz);
+    template<typename M, typename LM, typename A>
+    SegTreeLazy(M m, LM lm, A a, L dl = L())
+        : merge(m), lazy_merge(lm), apply(a), dlazy(dl)
+    {
+    }
 
-		_init(arr, 0, n - 1, 1);
-	}
+    void init(vector<T> arr)
+    {
+        n = (int)arr.size();
+        int sz = (1 << (clog2(n) + 1));
+        tree.resize(sz);
+        lazy.resize(sz, dlazy);
 
-	void update_range(int left, int right, i64 diff) { _update_range(0, n - 1, 1, left, right, diff); }
+        _init(arr, 0, n - 1, 1);
+    }
 
-	i64 query(int left, int right) { return _query(0, n - 1, 1, left, right); }
+    void update_range(int left, int right, L diff) { _update_range(0, n - 1, 1, left, right, diff); }
+
+    T query(int left, int right) { return _query(0, n - 1, 1, left, right); }
 
 private:
-	i64 _init(vector<i64>& arr, int start, int end, int node)
-	{
-		if (start == end)
-			return tree[node] = arr[start];
+    T _init(vector<T>& arr, int start, int end, int node)
+    {
+        if (start == end)
+            return tree[node] = arr[start];
 
-		int mid = (start + end) / 2;
+        int mid = (start + end) / 2;
 
-		return tree[node] = _init(arr, start, mid, node * 2) + _init(arr, mid + 1, end, node * 2 + 1);
-	}
+        return tree[node] = merge(_init(arr, start, mid, node * 2), _init(arr, mid + 1, end, node * 2 + 1));
+    }
 
-	void update_lazy(int start, int end, int node)
-	{
-		if (lazy[node] == 0)
-			return;
+    void update_lazy(int start, int end, int node)
+    {
+        if (lazy[node] == dlazy)
+            return;
 
-		tree[node] += (end - start + 1) * lazy[node];
+        tree[node] = apply(start, end, tree[node], lazy[node]);
 
-		if (start != end)
-		{
-			lazy[node * 2] += lazy[node];
-			lazy[node * 2 + 1] += lazy[node];
-		}
+        if (start != end)
+        {
+            lazy[node * 2] = lazy_merge(lazy[node * 2], lazy[node]);
+            lazy[node * 2 + 1] = lazy_merge(lazy[node * 2 + 1], lazy[node]);
+        }
 
-		lazy[node] = 0;
-	}
+        lazy[node] = dlazy;
+    }
 
-	void _update_range(int start, int end, int node, int left, int right, i64 diff)
-	{
-		update_lazy(start, end, node);
+    void _update_range(int start, int end, int node, int left, int right, L diff)
+    {
+        update_lazy(start, end, node);
 
-		if (left > end || right < start)
-			return;
+        if (left > end || right < start)
+            return;
 
-		if (left <= start && end <= right)
-		{
-			tree[node] += (end - start + 1) * diff;
+        if (left <= start && end <= right)
+        {
+            lazy[node] = diff;
+            update_lazy(start, end, node);
+            return;
+        }
 
-			if (start != end)
-			{
-				lazy[node * 2] += diff;
-				lazy[node * 2 + 1] += diff;
-			}
+        int mid = (start + end) / 2;
+        _update_range(start, mid, node * 2, left, right, diff);
+        _update_range(mid + 1, end, node * 2 + 1, left, right, diff);
+        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
+    }
 
-			return;
-		}
+    T _query(int start, int end, int node, int left, int right)
+    {
+        update_lazy(start, end, node);
 
-		int mid = (start + end) / 2;
-		_update_range(start, mid, node * 2, left, right, diff);
-		_update_range(mid + 1, end, node * 2 + 1, left, right, diff);
-		tree[node] = tree[node * 2] + tree[node * 2 + 1];
-	}
+        if (left > end || right < start)
+            return 0;
 
-	i64 _query(int start, int end, int node, int left, int right)
-	{
-		update_lazy(start, end, node);
+        if (left <= start && end <= right)
+            return tree[node];
 
-		if (left > end || right < start)
-			return 0;
+        int mid = (start + end) / 2;
+        return merge(_query(start, mid, node * 2, left, right),
+            _query(mid + 1, end, node * 2 + 1, left, right));
+    }
 
-		if (left <= start && end <= right)
-		{
-			return tree[node];
-		}
+    function<T(T, T)> merge;
+    function<L(L, L)> lazy_merge;
+    function<T(int, int, T, L)> apply;
+    L dlazy;
 
-		int mid = (start + end) / 2;
-		return _query(start, mid, node * 2, left, right) +
-			_query(mid + 1, end, node * 2 + 1, left, right);
-	}
-
-	int n = 0;
-	vector<i64> tree;
-	vector<i64> lazy;
+    int n = 0;
+    vector<T> tree;
+    vector<L> lazy;
 };
