65eb524283f131ec5bd94204c55d397c7262c5d4
namhyunuk <jwvg0425@naver.com>
2023-05-14 21:03:49 +0900

    change scc to class

--- cpp/SCC.cpp
+++ cpp/SCC.cpp
@@ -1,73 +1,96 @@
-vector<int> edges[10001];
-bool visited[10001]; // scc에 이미 포함된 경우 true
-int discover[10001];
-int cidx[10005];
-stack<int> s;
-vector<vector<int>> scc;
-int idx = 1;
 
-int dfs(int k)
+template<int SZ>
+struct Scc
 {
-	s.push(k);
+	vector<int> edges[SZ];
+	bool visited[SZ] = { false, }; // scc에 이미 포함된 경우 true
+	int discover[SZ] = { 0, };
+	int cidx[SZ] = { 0, };
+	// scc간 간선
+	vector<int> sedges[SZ];
+	stack<int> s;
+	vector<vector<int>> scc;
+	int idx = 1;
 
-	discover[k] = idx++;
-
-	int ret = discover[k];
-
-	for (auto& edge : edges[k])
+	int dfs(int k)
 	{
-		if (visited[edge])
-			continue;
+		s.push(k);
 
-		if (discover[edge] != 0)
+		discover[k] = idx++;
+
+		int ret = discover[k];
+
+		for (auto& edge : edges[k])
 		{
-			ret = min(discover[edge], ret);
-			continue;
+			if (visited[edge])
+				continue;
+
+			if (discover[edge] != 0)
+			{
+				ret = min(discover[edge], ret);
+				continue;
+			}
+
+			ret = min(dfs(edge), ret);
 		}
 
-		ret = min(dfs(edge), ret);
-	}
-
-	if (discover[k] == ret)
-	{
-		vector<int> newScc;
-
-		while (true)
+		if (discover[k] == ret)
 		{
-			int t = s.top();
-			s.pop();
+			vector<int> newScc;
 
-			visited[t] = true;
-			newScc.push_back(t);
+			while (true)
+			{
+				int t = s.top();
+				s.pop();
 
-			if (t == k)
-				break;
+				visited[t] = true;
+				newScc.push_back(t);
+
+				if (t == k)
+					break;
+			}
+
+			sort(newScc.begin(), newScc.end());
+
+			scc.push_back(newScc);
 		}
 
-		sort(newScc.begin(), newScc.end());
-
-		scc.push_back(newScc);
+		return ret;
 	}
 
-	return ret;
-}
-
-void calc_scc(int n)
-{
-	for (int i = 1; i <= n; i++)
+	void calc_scc(int n)
 	{
-		if (!visited[i])
-			dfs(i);
+		for (int i = 0; i < n; i++)
+		{
+			if (!visited[i])
+				dfs(i);
+		}
+
+		sort(all(scc), [](const vector<int>& l, const vector<int>& r)
+			{
+				return l[0] < r[0];
+			});
+
+		for (int i = 0; i < scc.size(); i++)
+		{
+			for (auto& v : scc[i])
+				cidx[v] = i;
+		}
+
+		for (int i = 0; i < scc.size(); i++)
+		{
+			for (auto& v : scc[i])
+			{
+				for (auto& e : edges[v])
+				{
+					if (cidx[e] == i)
+						continue;
+					sedges[i].push_back(cidx[e]);
+				}
+			}
+
+			sort(all(sedges[i]));
+			sedges[i].erase(unique(all(sedges[i])), sedges[i].end());
+		}
 	}
-
-	sort(all(scc), [](const vector<int>& l, const vector<int>& r)
-	{
-		return l[0] < r[0];
-	});
-
-	for (int i = 0; i < scc.size(); i++)
-	{
-		for (auto& v : scc[i])
-			cidx[v] = i;
-	}
-}
+};
